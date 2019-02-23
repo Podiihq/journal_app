5554725e03bb70e6d4e279e38e349dfacaa5e2e2
Infinical <inficalvin@gmail.com>
2019-02-23 16:58:28 +0300

    Sign up page

--- journal_app/lib/main.dart
+++ journal_app/lib/main.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
+import 'sign_up.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+ 
+ final routes = <String, WidgetBuilder>{
+   SignupPage.tag: (context) => SignupPage(),
+ };
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Journal App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      
+        primarySwatch: Colors.purple,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+
+      home: SignupPage(),
+      routes: routes,
     );
   }
 }
