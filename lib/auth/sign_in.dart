568833fe77c3ad8e3c9649aff910f944e8827eb3
Infinical <inficalvin@gmail.com>
2019-03-09 15:32:34 +0300

    Change folder structrure

---
+++ lib/auth/sign_in.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:journal_app/home_page.dart';
+
+class SignInPage extends StatefulWidget {
+  static String tag = 'signup-page';
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SignInPageState();
+  }
+}
+
+class _SignInPageState extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) {
+    final logo = CircleAvatar(
+      backgroundColor: Colors.cyanAccent,
+      child: Text(
+        "JA",
+        style: TextStyle(
+          fontSize: 85,
+          fontFamily: 'Roboto',
+        ),
+      ),
+      radius: 80.0,
+    );
+
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      decoration: InputDecoration(
+        labelText: 'Your email',
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0)),
+      ),
+    );
+
+    final password = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      decoration: InputDecoration(
+          labelText: 'Password',
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(15.0))),
+    );
+
+    final signupButton = Padding(
+        padding: EdgeInsets.symmetric(vertical: 20.0),
+        child: RaisedButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          onPressed: () {
+            Navigator.of(context).pushNamed(HomePage.tag);
+          },
+          padding: EdgeInsets.all(12),
+          color: Colors.purpleAccent,
+          child: Text(
+            'Log In',
+            style: TextStyle(color: Colors.white),
+          ),
+        ));
+
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: ListView(
+            shrinkWrap: true,
+            padding: EdgeInsets.only(left: 24.0, right: 24.0),
+            children: <Widget>[
+              logo,
+              SizedBox(height: 15.0),
+              email,
+              SizedBox(height: 15.0),
+              password,
+              SizedBox(height: 15.0),
+              signupButton
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/auth/sign_up.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:journal_app/home_page.dart';
+
+class SignUpNewUser extends StatefulWidget {
+  static String tag = 'login-page';
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SignUpNewUserState();
+  }
+}
+
+class _SignUpNewUserState extends State<SignUpNewUser> {
+  @override
+  Widget build(BuildContext context) {
+    final logo = CircleAvatar(
+      backgroundColor: Colors.cyanAccent,
+      child: Text(
+        "JA",
+        style: TextStyle(
+          fontSize: 85,
+          fontFamily: 'Roboto',
+        ),
+      ),
+      radius: 80.0,
+    );
+
+    final username = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      decoration: InputDecoration(
+        labelText: 'Username',
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0)),
+      ),
+    );
+
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      decoration: InputDecoration(
+        labelText: 'Email',
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0)),
+      ),
+    );
+
+    final password = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      decoration: InputDecoration(
+          labelText: 'Password',
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(15.0))),
+    );
+
+    final confirmPassword = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      decoration: InputDecoration(
+          labelText: 'Confirm Password',
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(15.0))),
+    );
+
+    final signupButton = Padding(
+        padding: EdgeInsets.symmetric(vertical: 15.0),
+        child: RaisedButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          onPressed: () {
+            Navigator.of(context).pushNamed(HomePage.tag);
+          },
+          padding: EdgeInsets.all(12),
+          color: Colors.purpleAccent,
+          child: Text(
+            'Sign Up',
+            style: TextStyle(color: Colors.white),
+          ),
+        ));
+
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: ListView(
+            shrinkWrap: true,
+            padding: EdgeInsets.only(left: 24.0, right: 24.0),
+            children: <Widget>[
+              logo,
+              SizedBox(
+                height: 25.0,
+              ),
+              username,
+              SizedBox(height: 25.0),
+              email,
+              SizedBox(height: 25.0),
+              password,
+              SizedBox(height: 25.0),
+              confirmPassword,
+              SizedBox(
+                height: 25.0,
+              ),
+              signupButton,
+            ],
+          ),
+        ));
+  }
+}
