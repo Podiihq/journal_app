08c1700658788068303d3562512f32317e1d83e4
keronei <keronei95@gmail.com>
2019-03-30 15:58:06 +0300

    Added Journal fetching from db using future

--- lib/database/db_manager.dart
+++ lib/database/db_manager.dart
@@ -3,10 +3,12 @@ import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'dart:io';
 import '../models/journal_model.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class JournalDatabase {
   /*
   Singleton instance to ensure consistency -- only one way of modifying the db
+
    */
   JournalDatabase._();
 
@@ -29,7 +31,7 @@ class JournalDatabase {
   initializeDB() async {
     //relation creator statement:
     var sql =
-        "CREATE TABLE IF NOT EXISTS journals(journal_id INTEGER AUTOINCREMENT PRIMARY KEY, journal_head TEXT,"
+        "CREATE TABLE IF NOT EXISTS journals(journal_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, journal_head TEXT,"
         " journal_entry TEXT, is_synced INTEGER, is_fav INTEGER);";
     Directory documentsInDevice = await getApplicationDocumentsDirectory();
     String path = join(documentsInDevice.path, "journal_entries.db");
@@ -43,9 +45,18 @@ class JournalDatabase {
     final dataBaseRef = await jdatabase;
 
     var operationResults = await dataBaseRef.rawInsert(
-        "INSERT INTO journals(journal_id, journal_head, "
-        "journal_entry, is_synced, is_fav) VALUES (${newEntry.journal_id}, ${newEntry.journal_head}, ${newEntry.journal_entry}, ${newEntry.is_synced},${newEntry.is_fav} )");
+        "INSERT INTO journals(journal_head, "
+        "journal_entry) VALUES ('${newEntry.journal_head}', '${newEntry.journal_entry}');");
+
+    print("INSERTION RESULTS " + operationResults.toString());
 
     return operationResults;
+  }
+
+  fetchJournals() async {
+    var readyDatabase = await jdatabase;
+    var forcedList = await readyDatabase.query("journals");
+
+    return forcedList;
   }
 }
