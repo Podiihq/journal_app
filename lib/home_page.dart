4a266a3464b3c72243f9904bc680bbe7d5c4753a
keronei <keronei95@gmail.com>
2019-03-30 16:00:22 +0300

    Implemented fetching journals from db and listing them from future using FutureBuilder

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import './models/journal_crud.dart';
+import './database/db_manager.dart';
+import './models/journal_model.dart';
 
 class JournalsList extends StatefulWidget {
   static String tag = 'journalsList';
@@ -24,14 +26,91 @@ class _JournalsListState extends State<JournalsList> {
 
   Widget _setFav(int position) {
     return (position % 2) == 0
-        ? Icon(
-            Icons.favorite_border,
+        ? IconButton(
+            icon: Icon(Icons.favorite_border),
             color: Colors.red,
+            onPressed: () {},
           )
         : Icon(
             Icons.favorite,
             color: Colors.red,
           );
+  }
+
+  Widget fetchJournals() {
+    //initialize the futureBuilder
+
+    return FutureBuilder(
+        future: JournalDatabase.db.fetchJournals(),
+        /*the future param takes the guy whom we are waiting for and passes the  data
+        to AsyncSnapshot when the data arrives, also note that the 
+       snapshot carries two params: 1. Information about the request and 2. the actual response
+       */
+        builder: (BuildContext context, AsyncSnapshot<dynamic> received) {
+          switch (received.connectionState) {
+            case ConnectionState.none:
+              return Text('Nothing is happening');
+            case ConnectionState.active:
+            case ConnectionState.waiting:
+              return Text('Awaiting result...');
+            case ConnectionState.done:
+              if (received.hasError) return Text('Error: ${received.error}');
+              /*
+              Once the request is successful, call the painter to put things in place.
+               */
+              return paintReceivedJournals(received.data);
+          }
+        });
+  }
+
+  paintReceivedJournals(List bunchOfJournals) {
+    /*
+    This function takes in a list as param and returns a
+    ListView.Builder that paints the homepage.
+     */
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int positionOfJournal) {
+        return Dismissible(
+          key: Key(bunchOfJournals[positionOfJournal]['journal_head']),
+          background: Container(
+            color: Colors.red,
+            padding: EdgeInsets.only(top: 25.0, right: 15.0),
+            child: Text(
+              'Delete',
+              style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30.0),
+              textAlign: TextAlign.right,
+            ),
+          ),
+          onDismissed: (DismissDirection swipedDir) {
+            setState(() {
+              // dummyJournals.remove(dummyJournals[positionOfJournal]);
+            });
+          },
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading: CircleAvatar(
+                  child: Text(
+                    bunchOfJournals[positionOfJournal]['journal_head'][0],
+                    style: TextStyle(color: Colors.deepOrange, fontSize: 18.0),
+                  ),
+                  backgroundColor: Theme.of(context).primaryColor,
+                ),
+                title: Text(
+                  bunchOfJournals[positionOfJournal]['journal_entry'],
+                  style: TextStyle(fontFamily: 'Oswald', fontSize: 17.0),
+                ),
+                subtitle:
+                    Text(bunchOfJournals[positionOfJournal]['journal_entry']),
+                trailing: _setFav(noteCount = noteCount + 1),
+              ),
+              Divider()
+            ],
+          ),
+        );
+      },
+      itemCount: bunchOfJournals.length,
+    );
   }
 
   @override
@@ -42,49 +121,12 @@ class _JournalsListState extends State<JournalsList> {
           title: Text('My Journals'),
           backgroundColor: Theme.of(context).primaryColor,
         ),
-        body: ListView.builder(
-          itemBuilder: (BuildContext context, int positionOfJournal) {
-            return Dismissible(
-              key: Key(dummyJournals[positionOfJournal]['head']),
-              background: Container(
-                color: Colors.red,
-                padding: EdgeInsets.only(top: 25.0, right: 15.0),
-                child: Text(
-                  'Delete',
-                  style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30.0),
-                  textAlign: TextAlign.right,
-                ),
-              ),
-              onDismissed: (DismissDirection swipedDir) {
-                setState(() {
-                  dummyJournals.remove(dummyJournals[positionOfJournal]);
-                });
-              },
-              child: Column(
-                children: <Widget>[
-                  ListTile(
-                    leading: CircleAvatar(
-                      child: Text(
-                        dummyJournals[positionOfJournal]['head'][0],
-                        style:
-                            TextStyle(color: Colors.deepOrange, fontSize: 18.0),
-                      ),
-                      backgroundColor: Theme.of(context).primaryColor,
-                    ),
-                    title: Text(
-                      dummyJournals[positionOfJournal]['head'],
-                      style: TextStyle(fontFamily: 'Oswald', fontSize: 17.0),
-                    ),
-                    subtitle: Text(dummyJournals[positionOfJournal]['date']),
-                    trailing: _setFav(noteCount = noteCount + 1),
-                  ),
-                  Divider()
-                ],
-              ),
-            );
-          },
-          itemCount: dummyJournals.length,
-        ),
+        body: fetchJournals(),
+        /*
+        The body is built in the future, this means that the data used to create the body 
+        is fetched from the db through an async method, which loads parallel with other 
+        events, the body therefore might have to wait for sometime before it can be painted. 
+         */
         floatingActionButton: FloatingActionButton(
           elevation: 6.0,
           onPressed: () {
